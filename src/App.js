f8acc296ce7e33c1e8ba568946aa1fe6b260c2fa
Chrisz <cjmulyawan@gmail.com>
2022-11-29 22:33:42 +0700

    commit awal tubes repel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Card, IconButton, List, Paper, Typography } from "@mui/material";
+import ListItemProduct from "./components/ListItemProduct";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+const BASE_API_URL = `https://dummyjson.com`;
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      await axios
+        .get(`${BASE_API_URL}/products/category/fragrances`)
+        .then((res) => {
+          const responseData = res.data.products;
+          setProducts(responseData);
+        })
+        .catch((error) => {
+          console.log(error);
+          window.alert(error);
+        });
+    }
+
+    getProducts();
+  }, []);
+
+
+  const handleDeleteProducts = (id, idx) => {
+    async function delProducts() {
+      await axios
+        .delete(`${BASE_API_URL}/products/${id}`)
+        .then((res) => {
+          let arr = products;
+          if (idx !== -1) {
+            arr.splice(idx, 1)
+          }
+          setProducts([...arr]);
+        })
+        .catch((error) => {
+          console.log(error);
+          window.alert(error);
+        })
+    }
+
+    delProducts();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="list-container">
+        <div className="list-title-wrapper">
+          <Typography variant="h4">Passenger</Typography>
+        </div>
+        <Card elevation={2} style={{ maxHeight: "700px", overflow: "auto" }}>
+          <List>
+            {products.map((d, idx) => (
+              <ListItemProduct
+                key={d.id}
+                image={d.thumbnail}
+                primaryText={`$${d.price} ${d.title}`}
+                secondaryText={`${d.description}`}
+                rating = {`${d.rating}`}
+                onDelete={() => handleDeleteProducts(d.id, idx)}
+              />
+            ))}
+          </List>
+        </Card>
+      </div>
     </div>
   );
 }
---
+++ src/components/ListItemProduct.js
@@ -0,0 +1,30 @@
+import { Delete, RoundedCorner } from "@mui/icons-material";
+import {
+  Avatar,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+  IconButton
+} from "@mui/material";
+import React from "react";
+
+function ListItemProducts({ image, primaryText, secondaryText, rating, onDelete }) {
+  return (
+    <ListItem>
+      <ListItemAvatar sx={{ marginRight: 2 }}>
+        <Avatar variant="circle" alt="avatar" src={image} sx={{ width: 75, height: 75  }}/>
+      </ListItemAvatar>
+      <ListItemText
+        primary={<Typography variant="h6">{primaryText}</Typography>}
+        secondary={<Typography variant="p">{secondaryText}</Typography>}
+      />
+      <Typography sx={{fontWeight: 'bold'}} variant="h8">{rating}</Typography>
+      <IconButton size="large" onClick={onDelete}>
+        <Delete />
+      </IconButton>
+    </ListItem>
+  );
+}
+
+export default ListItemProducts;
